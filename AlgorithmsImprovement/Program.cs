00ca202c4b3cf59ef2d8d85d9149a808bdd920ba
rameshFIT <47829632+rameshFIT@users.noreply.github.com>
2020-06-25 00:00:41 -0400

    Added Question as comments

--- AlgorithmsImprovement/Program.cs
+++ AlgorithmsImprovement/Program.cs
@@ -13,6 +13,15 @@ namespace AlgorithmsImprovement
             //end time 6:41PM
         }
 
+        /*Say you have an array for which the nth element is the price of a given day n.
+        If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), 
+        design an algorithm to find the maximum profit. Note that you cannot sell a stock before you buy one.
+        Example 1) 
+        Input: {7,1,5,3,6,4}
+        Output: 5
+        Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+        Not 7-1 = 6, as selling price needs to be larger than buying price. */
+
         private static void GetMaxProfit(int[] inputPrices)
         {
             int highestProfitNumber = 0;
